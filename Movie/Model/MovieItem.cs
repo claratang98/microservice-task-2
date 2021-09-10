2126440290cd06cc77299fc556820da21f03dfa9
Ga Yi Clara Tang <tanggy@simtech.a-star.edu.sg>
2021-09-10 15:44:27 +0800

    First Commit

---
+++ Movie/Model/MovieItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Model
+{
+    public class MovieItem
+    {   
+        public long Id { get; set; }
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+        public string Venue { get; set; }
+        public long Ticket { get; set; }
+        public string Currency { get; set; }
+        public long Amount { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
