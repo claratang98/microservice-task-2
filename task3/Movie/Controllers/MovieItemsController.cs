cc019575f5aa5f486efd766b8f628f2b2fef2fdf
Ga Yi Clara Tang <tanggy@simtech.a-star.edu.sg>
2021-09-10 17:16:36 +0800

    Redid

---
+++ task3/Movie/Controllers/MovieItemsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie.Model;
+
+namespace MovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieItemsController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MovieItemsController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MovieItems
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<MovieItem>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/MovieItems/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MovieItem>> GetMovieItem(long id)
+        {
+            var movieItem = await _context.Movies.FindAsync(id);
+
+            if (movieItem == null)
+            {
+                return NotFound();
+            }
+
+            return movieItem;
+        }
+
+        // PUT: api/MovieItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PutMovieItem(long id, MovieItem movieItem)
+        {
+            if (id != movieItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movieItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MovieItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MovieItem>> PostMovieItem(MovieItem movieItem)
+        {
+            _context.Movies.Add(movieItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovieItem", new { id = movieItem.Id }, movieItem);
+        }
+
+        // DELETE: api/MovieItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovieItem(long id)
+        {
+            var movieItem = await _context.Movies.FindAsync(id);
+            if (movieItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movieItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieItemExists(long id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ task3/Movie/Model/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Model
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieItem> Movies { get; set; }
+    }
+}
